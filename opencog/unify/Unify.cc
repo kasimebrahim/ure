89380e9d974031f99a358dd43b5ff696e1d4ad2b
kasimebrahim <se.kasim.ebrahim@gmail.com>
2019-08-20 12:34:10 +0300

    Implement unordered glob unify for lhs glob_node

--- opencog/unify/Unify.cc
+++ opencog/unify/Unify.cc
@@ -601,9 +601,11 @@ Unify::unordered_glob_unify(const HandleSeq &lhs, const HandleSeq &rhs,
 		Context lhs_context, Context rhs_context) const
 {
 	Arity lhs_arity(lhs.size());
+	Arity rhs_arity(rhs.size());
 
 	SolutionSet sol;
 	for (Arity i = 0; i < lhs_arity; ++i) {
+		if(!rhs_arity) return sol;
 		auto head_sol = unify(lhs[i], rhs[0], lhs_context, rhs_context);
 		if (head_sol.is_satisfiable()) {
 			HandleSeq lhs_tail;
@@ -630,6 +632,32 @@ Unify::unordered_glob_unify(const HandleSeq &lhs, const HandleSeq &rhs,
 							break;
 						}
 						lhs_tail = HandleSeq(lhs);
+						head_sol.clear();
+					}
+				} while (!tail_sol.is_satisfiable());
+			}
+			if (lhs[i]->get_type() == GLOB_NODE) {
+				SolutionSet tail_sol;
+				lhs_tail = HandleSeq(lhs);
+				rhs_tail = HandleSeq(cp_erase(rhs, 0));
+				do {
+					tail_sol = unordered_glob_unify(lhs_tail, rhs_tail, lhs_context, rhs_context);
+					if (tail_sol.is_satisfiable()) {
+						// since join is not implemented for GLOB_NODE I am Using insert for now
+						// SolutionSet perm_sol = join(head_sol, tail_sol);
+						SolutionSet perm_sol;
+						perm_sol.insert(head_sol.begin(), head_sol.end());
+						perm_sol.insert(tail_sol.begin(), tail_sol.end());
+
+						// Union merge satisfiable permutations
+						sol.insert(perm_sol.begin(), perm_sol.end());
+					} else {
+						lhs_tail = HandleSeq(cp_erase(lhs, i));
+						if (lhs_tail.size() == rhs_tail.size() && rhs_tail.size() == 0) {
+							sol.insert(head_sol.begin(), head_sol.end());
+							break;
+						}
+						rhs_tail = HandleSeq(rhs);
 						head_sol.clear();
 					}
 				} while (!tail_sol.is_satisfiable());
